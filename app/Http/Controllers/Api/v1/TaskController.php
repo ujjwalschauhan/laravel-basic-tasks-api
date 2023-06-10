945c33905c8694395813bbe0da5094d01a9be4cf
ujjwalschauhan <4325uschauhan@gmail.com>
2023-06-10 22:09:07 +0530

    add: delete task api

--- app/Http/Controllers/Api/v1/TaskController.php
+++ app/Http/Controllers/Api/v1/TaskController.php
@@ -63,6 +63,8 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
