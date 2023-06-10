037d2e73bbe1d22d45de3c09ec6f4d4e4393a32b
ujjwalschauhan <66547370+ujjwalschauhan@users.noreply.github.com>
2023-06-10 10:19:25 +0530

    Dev (#4)
    * add: save end point for task
    
    * add: make end point to update task
    
    * add: end point to mark task complete

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\CompleteTaskController;
 use App\Http\Controllers\Api\v1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::prefix('v1')->group(function(){
     Route::apiResource('/tasks', TaskController::class);
+    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
